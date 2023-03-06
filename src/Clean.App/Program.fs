77060b5bad723736fd16b18ab38847c295fc4a93
Misha Gorshenin <gorshenin.mv@gmail.com>
2023-03-06 23:05:53 +0500

    add tests

--- src/Clean.App/Program.fs
+++ src/Clean.App/Program.fs
@@ -9,26 +9,25 @@ module Clean =
   let inline combine path1 path2 = Path.Combine(path1, path2)
   let inline (</>) path1 path2 = combine path1 path2
 
-  let private pickOldFiles (files: seq<FileInfo>) =
+  let private pickOldFiles (files: seq<IFileInfo>) =
     let now = DateTime.Now.ToUniversalTime()
     files |> Seq.filter (fun file -> file.LastWriteTimeUtc.AddDays(30) < now)
 
-  let private removeOldFiles (files: seq<FileInfo>) =
-    files |> Seq.iter (fun file -> file.Delete())
+  let private removeOldFiles (files: seq<IFileInfo>) =
+    files |> pickOldFiles |> Seq.iter (fun file -> file.Delete())
 
-  let private getDirInfo path =
-    if Directory.Exists(path) then
-      let dirInfo = DirectoryInfo(path)
+  let private getDirInfo (fs: IFileSystem) path =
+    if fs.Directory.Exists(path) then
+      let dirInfo = fs.DirectoryInfo.New(path)
       Some dirInfo
     else
       None
 
   let clean (fs: IFileSystem) =
-    let path = "D:" </> "tmp"
+    let path = "/temp"
 
     maybe {
-      let! dirInfo = getDirInfo path
-      let dirInfo = DirectoryInfo(path)
+      let! dirInfo = getDirInfo fs path
       let opts = EnumerationOptions()
       opts.RecurseSubdirectories <- true
       dirInfo.GetFiles("*.*", opts) |> removeOldFiles
--- src/Clean.Tests/Integration.fs
+++ src/Clean.Tests/Integration.fs
@@ -4,13 +4,37 @@ open NUnit
 open NUnit.Framework
 open FsUnit
 
+open System
+open System.Linq
 open System.IO.Abstractions.TestingHelpers
 
 open Clean.App
 
 module Integration =
 
+  let createMockFileData modifiedDate =
+    let ret = MockFileData([||])
+    ret.LastWriteTime <- modifiedDate
+    ret
+
+  let createInitialFs () =
+    let oldDate = DateTimeOffset.UtcNow.AddDays(-40.0)
+    let files = dict [
+      ("/temp" </> "oldemptydir", createMockFileData oldDate);
+      ("/temp" </> "olddirwithcontent" </> "file1.json", createMockFileData oldDate);
+      ("/temp" </> "olddirwithcontent" </> "nesteddir" </> "file1.json", createMockFileData oldDate);
+    ]
+
+    let fs = MockFileSystem(files)
+    fs
+
   [<Test>]
-  let ``clean with empty folder should work`` () =
+  let ``clean with not existed directory should work`` () =
     let fs = MockFileSystem()
     clean fs
+
+  [<Test>]
+  let ``clean old files`` () =
+    let fs = createInitialFs ()
+    clean fs
+    fs.AllFiles.Count() |> should equal 0
